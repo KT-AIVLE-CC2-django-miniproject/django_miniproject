8d3f7a1f64feda448bf1c3d289419babc74766e1
userYJ <yeongjae0213@naver.com>
2022-01-25 12:49:58 +0900

    post(게시판 작성 , 제목 +내용) +추가수정 필요

--- candyprj/boardapp/urls.py
+++ candyprj/boardapp/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = 'boardapp'
 urlpatterns = [
-    path('home/', views.home, name ='home'),
+    path('home', views.home, name ='home'),
+    path('post', views.post, name ='post'),
     # path('<int:board_id>/', views.detail, name ='detail'),
     # path('create/', views.create, name ='create'),
     # path('create/write_board', views.write_board, name ='write_board'),
