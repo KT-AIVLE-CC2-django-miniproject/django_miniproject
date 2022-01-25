8e8e8de068a71aedb8da0f6fb706921297ae108e
Jarvis-bin <wjdqlsdlekt1@gmail.com>
2022-01-25 09:44:57 +0900

    trouble shooting

--- candy/Board/views.py
+++ candy/Board/views.py
@@ -12,18 +12,18 @@ def home(request):
     paginator = Paginator(all_boards, 10)
     page = int(request.GET.get('page',1))
     board_list = paginator.get_page(page)
-    return render(request, 'board/base.html', {'title': 'Board List',
+    return render(request, 'Board/base.html', {'title': 'Board List',
     'board_list' : board_list})
 
 def detail(request, postNum):
     board = Board.objects.get(id=postNum)
-    return render(request, 'board/detail.html', {'board':board})
+    return render(request, 'Board/detail.html', {'board':board})
 
 def create(request):
-    return render(request, 'board/create.html')
+    return render(request, 'Board/create.html')
 
 def write_board(request, postNum):
     wboard = Board(title=request.POST['title'], 
     content = request.POST['detail'], id = 'kim', pub_date=timezone.now())
     wboard.save()
-    return HttpResponseRedirect(reverse('board:detail'))
+    return HttpResponseRedirect(reverse('Board:detail'))
