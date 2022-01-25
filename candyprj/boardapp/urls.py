a7f35d57446575531a20e8780736e28573ac7303
Jarvis-bin <wjdqlsdlekt1@gmail.com>
2022-01-25 11:17:16 +0900

    boardapp home

--- candyprj/boardapp/urls.py
+++ candyprj/boardapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'boardapp'
 urlpatterns = [
-    # path('', views.home, name ='home'),
-    # path('<int:board_id>/', views.detail, name ='detail'),
+    path('', views.home, name ='home'),
+    path('<int:board_id>/', views.detail, name ='detail'),
     # path('create/', views.create, name ='create'),
     # path('create/write_board', views.write_board, name ='write_board'),
     # #path('<int:board_id>/create_reply', views.creat_reply, name ='create_reply'),
--- candyprj/boardapp/views.py
+++ candyprj/boardapp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from audioop import reverse
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+from django.core.paginator import Paginator
+from .models import Board
 # Create your views here.
+def home(request):
+    all_boards = Board.objects.all().order_by("-pub_date")
+    paginator = Paginator(all_boards, 10)
+    page = int(request.GET.get('page',1))
+    board_list = paginator.get_page(page)
+    return render(request, 'boardapp/home.html', {'title': 'Board List',
+    'board_list' : board_list})
+
+def detail(request, postNum):
+    board = Board.objects.get(id=postNum)
+    return render(request, 'boardapp/detail.html', {'board':board})
+
+def create(request):
+    return render(request, 'boardapp/create.html')
+
+def write_board(request, postNum):
+    wboard = Board(title=request.POST['title'], 
+    content = request.POST['detail'], id = 'kim', pub_date=timezone.now())
+    wboard.save()
+    return HttpResponseRedirect(reverse('board:detail'))
