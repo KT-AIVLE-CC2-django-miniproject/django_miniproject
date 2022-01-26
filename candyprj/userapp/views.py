e2344607608915ffca5dbe8d05598fe5cfdf8b80
RYUBoY <ryusla@naver.com>
2022-01-26 14:17:07 +0900

    회원정보 수정 추가

--- candyprj/userapp/views.py
+++ candyprj/userapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from audioop import reverse
 from django.shortcuts import render,redirect
 from .models import User
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 def signup(request):
     if request.method == 'POST':
         id = request.POST.get('id')
@@ -24,21 +26,26 @@ def profile(request):
     return render(request, 'userapp/profile.html',{'profile':profile})
 
 
-# from .forms import UserUpdateForm
-# def update(request):
-#     if request.method == "POST":
-#         form = UserUpdateForm(request.POST, instance=request.user)  # 이게 없으면 수정할 때마다 새로운 계정을 만든다.
-#         if form.is_valid():
-#             form.save()  # 폼값을 불러오고 저장
-#             return redirect('user/profile/')
-#     else:
-#         form = UserUpdateForm(instance=request.user)
-#     return render(request, 'userapp/update.html', {'form': form})
+
+def update(request):
+    update = User.objects.get(id= 'abc')
+    if request.method == "POST":
+        
+        update.pw = request.POST.get('pw')
+        update.name = request.POST.get('name')
+        update.birth = request.POST.get('birth')
+        update.mail = request.POST.get('mail')
+       
+        update.save()
+        # return HttpResponseRedirect(reverse('profile'))
+        return render(request, 'userapp/profile.html')
+
+    else:
+        update = User
+        return render(request, 'userapp/update.html', {'update':update})
 
 
 
-
-  
 def login(request):
     if request.method == 'POST':
         id = request.POST.get('id')
