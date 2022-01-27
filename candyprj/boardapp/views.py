e10b5b2b183161399e1868cb767842f778d5d7f9
userYJ <yeongjae0213@naver.com>
2022-01-27 11:32:16 +0900

    aaa

--- candyprj/boardapp/views.py
+++ candyprj/boardapp/views.py
@@ -47,9 +47,7 @@ def write_board(request): #쓰기 페이지에서 글 등록시 submit 처리
     uid = request.session['id']
     uid = User.objects.get(id = uid)
 
-    cid = request.session['id']
-    cid = Board.objects.get(id=cid)
-    
+
     b = Board(id = uid,title=request.POST['title'], content=request.POST['detail'], 
     pub_date=timezone.now()) #recuritment --> 필요없다. 조회할때만 모집중인지 아닌지 버튼으로 ex 좋아요, 싫어요
     b.save()
