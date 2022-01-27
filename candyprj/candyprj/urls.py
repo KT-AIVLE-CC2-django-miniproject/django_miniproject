b201cf6d6df73a1351af663c67d7037f5ca3053a
userYJ <yeongjae0213@naver.com>
2022-01-27 15:41:45 +0900

    새로운페이지

--- candyprj/candyprj/urls.py
+++ candyprj/candyprj/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+#신규작성
+# from boardapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('board/', include('boardapp.urls')),
     path('user/', include('userapp.urls')),
+
+    # path('board/new/' , views.new_topic, name='new_topoc'), #게시글 작성 페이지 url 추가 board/new/ ?
 ]
