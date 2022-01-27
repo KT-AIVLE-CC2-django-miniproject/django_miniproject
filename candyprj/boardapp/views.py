37a9551af3e906df16a39d4fe5f881b99a572787
userYJ <yeongjae0213@naver.com>
2022-01-27 19:28:50 +0900

    detail1,update1

--- candyprj/boardapp/views.py
+++ candyprj/boardapp/views.py
@@ -145,28 +145,38 @@ def new_topic(request):
     return render(request,'boardapp/new_topic.html',{'topics': topics})
 
 
-# def detail(request, postNum): #게시글 제목 선택시 상세 페이지로 이동
-#     board = Board.objects.get(postNum=postNum)
-#     return render(request, 'boardapp/detail.html', {'boardapp': board})
+def detail1(request, writter): #게시글 제목 선택시 상세 페이지로 이동
+    board = Topic.objects.get(writter=writter)
+    return render(request, 'boardapp/detail1.html', {'boardapp': board})
 
-# def create_reply(request, postNum): # 상세 페이지에서 댓글 동록시 submit 처리
-#     uid = request.session['id']
-#     uid = User.objects.get(id=uid)
+def create1_reply(request, writter): # 상세 페이지에서 댓글 동록시 submit 처리
+    uid = request.session['id']
+    uid = User.objects.get(id=uid)
 
-# def update(request, board_id):
-#     b = Board.objects.get(postNum= board_id)
-#     temp = Board.objects.get(postNum= board_id)
-#     if request.method == "POST":
-#         b.title=request.POST['title']
-#         if b.title == "":
-#             b.title = temp.title
-#         b.content=request.POST['detail']
-#         if b.content == "":
-#             b.content = temp.content
-#         b.pub_date=timezone.now()
-#         b.save()
-#         return HttpResponseRedirect(reverse('boardapp:detail',args=(board_id,)))
-#     else:
-#         b=Board
-#         return render(request, 'boardapp/update.html', {'boardapp':b})
+    b = Topic.objects.get(writter = writter)
+    b.reply1_set.create(writter = uid, message=request.POST['message'], last_updated=timezone.now())
+    return HttpResponseRedirect(reverse('boardapp:detail1', args=(writter,)))  
+
+
+def update1(request, board_id):
+    b = Topic.objects.get(writter= board_id)
+    temp = Topic.objects.get(writter= board_id)
+    if request.method == "POST":
+        b.subject=request.POST['subject']
+        if b.subject == "":
+            b.subject = temp.subject
+        b.message=request.POST['message']
+        if b.message == "":
+            b.message = temp.message
+        b.last_updated=timezone.now()
+        b.save()
+        return HttpResponseRedirect(reverse('boardapp:detail1',args=(board_id,)))
+    else:
+        b=Topic
+        return render(request, 'boardapp/update1.html', {'boardapp':b})
+
+def delete1(request, board_id):
+    b = Topic.objects.get(writter=board_id)
+    b.delete()
+    return redirect('boardapp:share')
 
