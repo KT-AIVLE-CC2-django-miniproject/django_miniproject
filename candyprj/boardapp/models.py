b2c7bf1101c037fdbde03c4355455954af5d7148
RYUBoY <ryusla@naver.com>
2022-01-28 13:00:51 +0900

    aa

--- candyprj/boardapp/models.py
+++ candyprj/boardapp/models.py
@@ -16,7 +16,7 @@ class Board(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length= 1000)
     pub_date = models.DateTimeField()
-    imgfile = models.ImageField(null=True, upload_to="", blank=True)
+    # imgfile = models.ImageField(null=True, upload_to="", blank=True)
     def __str__(self):
         return self.title
         
